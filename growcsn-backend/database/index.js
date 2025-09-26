373050fd3dc583825ac37f1dc4040710fbc58198
Ahsan Faisal <61789531+AUAboi@users.noreply.github.com>
2025-09-26 18:23:00 +0500

    Update index.js

--- growcsn-backend/database/index.js
+++ growcsn-backend/database/index.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const CrashSeed = require('./models/CrashSeed');
+const crypto = require('crypto');
+const RollSeed = require("./models/RollSeed"); // adjust path as needed
+
 
 // Set mongoose mode to strict and deactive auto indexing
 mongoose.set('strictQuery', true);
@@ -10,7 +14,8 @@ const connectDB = async() => {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
-
+        await seedCrashSeedIfEmpty()
+        await seedRollSeedIfEmpty()
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch(err) {
         console.error(`Error: ${err.message}`);
@@ -18,4 +23,30 @@ const connectDB = async() => {
     }
 }
 
+const seedCrashSeedIfEmpty = async () => {
+  const count = await CrashSeed.countDocuments({ state: "created" });
+  if (count === 0) {
+    await CrashSeed.create({
+      seedPublic: crypto.randomBytes(8).toString("hex"),
+      seedServer: crypto.randomBytes(24).toString("hex"),
+      hash: crypto.createHash("sha256").update(crypto.randomBytes(24)).digest("hex"),
+      state: "created",
+    });
+    console.log("Initial CrashSeed created!");
+  }
+}
+const seedRollSeedIfEmpty = async () => {
+  const count = await RollSeed.countDocuments({ state: "created" });
+  if (count === 0) {
+    const serverSeed = crypto.randomBytes(24).toString("hex");
+    await RollSeed.create({
+      seedPublic: crypto.randomBytes(8).toString("hex"),
+      seedServer: serverSeed,
+      hash: crypto.createHash("sha256").update(serverSeed).digest("hex"),
+      state: "created", // optional because default is already 'created'
+    });
+    console.log("Initial RollSeed created!");
+  }
+};
 module.exports = connectDB;
+
